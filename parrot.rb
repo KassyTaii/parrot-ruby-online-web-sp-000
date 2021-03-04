80fa3441801e862de237a3155c65decb64e22c43
Kassandra Guerrero <kassandra-g@hotmail.com>
2021-03-04 03:22:56 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(string="Squawk!")
-  puts "#{string}"
-  "#{string}"
+
+
+def parrot(string)
+string = Squawk!
+puts "Squawk!"
 end
